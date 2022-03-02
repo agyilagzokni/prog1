8ba9282d5e7aeb9aa4902a67ca945d94da7a8a75
mdlolxd01@gmail.com <mdlolxd01@gmail.com>
2022-03-02 11:05:08 +0100

    kutya

--- ch14drill/ch14drill.cpp
+++ ch14drill/ch14drill.cpp
@@ -3,13 +3,32 @@
 	struct B1{
 		virtual void vf() {cout<< "B1::vf()\n";}
 		void f() {cout<< "B1::f()\n";}
+		virtual void pvf() =0;
 	};
 
 	struct D1 : B1{
 		void vf() override {cout << "D1::vf()\n";}
 		void f() {cout << "D1::f()\n";}
 	};
+	struct D2 : D1{
+		void pvf() override {cout <<"D2::pvf()\n";}
+	};
+	struct B2{
+		virtual void pvf() =0;
+	};
+	struct D21 : B2{
+		string s = "amongus";
+		void pvf() override {cout << s << "\n";}
+	};
+	struct D22 : B2{
+		int i = 3;
+		void pvf() override {cout << i << "\n";}
+	};
+	void f(B2& b){
+		b.pvf();
+	}
 int main(){
+	/*
 	//1.
 	cout << "1.\n"; 
 	B1 b;
@@ -27,6 +46,37 @@ int main(){
 	b1ref.f(); 
 	//4.
 	cout << "4.\n";
+	b.vf();
+	b.f();
+	d.vf();
+	d.f();
+	b1ref.vf();
+	b1ref.f(); 
+	//5.
 
+	/*
+	b.vf();
+	b.f();
+	d.vf();
+	d.f();
+	b1ref.vf();
+	b1ref.f();
+	*/
+	cout << "5.\n";
+	cout << "full error\n";
+	//6.
+	cout << "6.\n";
+	D2 d2;
+	d2.f();
+	d2.vf();
+	d2.pvf();
+	//7.
+	cout << "7.\n";
+	D21 d21;
+	D22 d22;
+	cout << "d21:\n";
+	f(d21);
+	cout << "d22:\n";
+	f(d22);
 	return 0;
 }
